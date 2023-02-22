dbec62a3459d4af35a5f879436cd5038c5a9ffb9
galetie <jarrydrocks@gmail.com>
2023-02-22 12:22:13 +0800

    Literally all of it

--- WikiApp/Form1.Designer.cs
+++ WikiApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WikiApp
 {
-    partial class Form1
+    partial class WikiApp
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listViewDisplay = new System.Windows.Forms.ListView();
+            this.itemName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Category = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDefinition = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxStructure = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusStripLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewDisplay
+            // 
+            this.listViewDisplay.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.itemName,
+            this.Category});
+            this.listViewDisplay.HideSelection = false;
+            this.listViewDisplay.Location = new System.Drawing.Point(271, 12);
+            this.listViewDisplay.Name = "listViewDisplay";
+            this.listViewDisplay.Size = new System.Drawing.Size(262, 426);
+            this.listViewDisplay.TabIndex = 0;
+            this.listViewDisplay.UseCompatibleStateImageBehavior = false;
+            this.listViewDisplay.View = System.Windows.Forms.View.Details;
+            this.listViewDisplay.SelectedIndexChanged += new System.EventHandler(this.listViewDisplay_SelectedIndexChanged);
+            // 
+            // itemName
+            // 
+            this.itemName.Text = "Item Name";
+            this.itemName.Width = 124;
+            // 
+            // Category
+            // 
+            this.Category.Text = "Category";
+            this.Category.Width = 138;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(6, 74);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(237, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(6, 19);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(87, 19);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonSave);
+            this.groupBox1.Controls.Add(this.buttonLoad);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 49);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Save and Load";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxDefinition);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxStructure);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBoxCategory);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.buttonDelete);
+            this.groupBox2.Controls.Add(this.buttonEdit);
+            this.groupBox2.Controls.Add(this.textBoxName);
+            this.groupBox2.Controls.Add(this.buttonAdd);
+            this.groupBox2.Location = new System.Drawing.Point(12, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(253, 316);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Create Update Delete";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Definition";
+            // 
+            // textBoxDefinition
+            // 
+            this.textBoxDefinition.Location = new System.Drawing.Point(6, 214);
+            this.textBoxDefinition.Multiline = true;
+            this.textBoxDefinition.Name = "textBoxDefinition";
+            this.textBoxDefinition.Size = new System.Drawing.Size(237, 96);
+            this.textBoxDefinition.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Structure";
+            // 
+            // textBoxStructure
+            // 
+            this.textBoxStructure.Location = new System.Drawing.Point(6, 168);
+            this.textBoxStructure.Name = "textBoxStructure";
+            this.textBoxStructure.Size = new System.Drawing.Size(237, 20);
+            this.textBoxStructure.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Category";
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(6, 120);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(237, 20);
+            this.textBoxCategory.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(168, 19);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(87, 19);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 1;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(6, 19);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.buttonSort);
+            this.groupBox3.Controls.Add(this.buttonSearch);
+            this.groupBox3.Location = new System.Drawing.Point(12, 67);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(171, 49);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Search and Sort";
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(87, 19);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 1;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(6, 19);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusStripLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 446);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(544, 22);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // statusStripLabel
+            // 
+            this.statusStripLabel.Name = "statusStripLabel";
+            this.statusStripLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // WikiApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(544, 468);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listViewDisplay);
+            this.Name = "WikiApp";
+            this.Text = "WikiApp";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewDisplay;
+        private System.Windows.Forms.ColumnHeader itemName;
+        private System.Windows.Forms.ColumnHeader Category;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDefinition;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxStructure;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusStripLabel;
     }
 }
 
--- WikiApp/Form1.cs
+++ WikiApp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,354 @@ using System.Windows.Forms;
 
 namespace WikiApp
 {
-    public partial class Form1 : Form
+    public partial class WikiApp : Form
     {
-        public Form1()
+        const string fileName = "definitions.dat";
+        const int rows = 12;
+        const int columns = 4;
+        string[,] wiki = new string[rows, columns];
+        public WikiApp()
         {
             InitializeComponent();
+            initializeWiki();
+        }
+
+        private void initializeWiki()
+        {
+            // Initialize the wiki array with empty strings
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    wiki[row, column] = "";
+                }
+            }
+        }
+
+        private void updateStatus(string status)
+        {
+            statusStripLabel.Text = status;
+        }
+
+        private void updateDisplay()
+        {
+            // Clear the display
+            listViewDisplay.Items.Clear();
+
+            // Add items to the display
+            for (int row = 0; row < rows; row++)
+            {
+                // Populate the item
+                ListViewItem item = new ListViewItem(wiki[row, 0]);
+                item.SubItems.Add(wiki[row, 1]);
+
+                // Add the item
+                listViewDisplay.Items.Add(item);
+            }
+        }
+
+        private void clearFields()
+        {
+            // Reset each field to an empty string
+            textBoxName.Text = "";
+            textBoxCategory.Text = "";
+            textBoxStructure.Text = "";
+            textBoxDefinition.Text = "";
+        }
+
+        private void displayFieldData(int index)
+        {
+            // Update input fields to reflect the data
+            textBoxName.Text = wiki[index, 0];
+            textBoxCategory.Text = wiki[index, 1];
+            textBoxStructure.Text = wiki[index, 2];
+            textBoxDefinition.Text = wiki[index, 3];
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // Validate all form data
+            string name = textBoxName.Text;
+            string category = textBoxCategory.Text;
+            string structure = textBoxStructure.Text;
+            string definition = textBoxDefinition.Text;
+            if (string.IsNullOrEmpty(name) ||
+                string.IsNullOrEmpty(category) ||
+                string.IsNullOrEmpty(structure) ||
+                string.IsNullOrEmpty(definition))
+            {
+                updateStatus("Invalid field input");
+                return;
+            }
+
+            // If everything is valid, insert the new item
+            // Find an empty spot to insert in to
+            for (int row = 0; row < rows; row++)
+            {
+                if (string.IsNullOrEmpty(wiki[row, 0]))
+                {
+                    // Found a spot to insert to
+                    wiki[row, 0] = name;
+                    wiki[row, 1] = category;
+                    wiki[row, 2] = structure;
+                    wiki[row, 3] = definition;
+
+                    // Inserted so exit
+                    updateStatus("Wiki item inserted");
+                    clearFields();
+                    updateDisplay();
+                    return;
+                }
+            }
+
+            // If loop finishes, wiki is full
+            updateStatus("Unable to insert wiki item. Wiki is full.");
+
+        }
+
+        private void listViewDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex;
+            // Set the text box fields
+            if (listViewDisplay.SelectedIndices.Count > 0)
+            {
+                // What index in the list has the user selected
+                selectedIndex = listViewDisplay.SelectedIndices[0];
+
+                // Display the data in the input field boxes
+                displayFieldData(selectedIndex);
+            }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            // Validate input
+            string name = textBoxName.Text;
+            string category = textBoxCategory.Text;
+            string structure = textBoxStructure.Text;
+            string definition = textBoxDefinition.Text;
+            if (string.IsNullOrEmpty(name) ||
+                string.IsNullOrEmpty(category) ||
+                string.IsNullOrEmpty(structure) ||
+                string.IsNullOrEmpty(definition))
+            {
+                updateStatus("Invalid field input");
+                return;
+            }
+
+            // Update item
+            int selectedIndex;
+
+            // Get the current selected index
+            if (listViewDisplay.SelectedIndices.Count > 0)
+            {
+                // What index in the list has the user selected
+                selectedIndex = listViewDisplay.SelectedIndices[0];
+
+                // Update input fields to reflect the data
+                wiki[selectedIndex, 0] = textBoxName.Text;
+                wiki[selectedIndex, 1] = textBoxCategory.Text;
+                wiki[selectedIndex, 2] = textBoxStructure.Text;
+                wiki[selectedIndex, 3] = textBoxDefinition.Text;
+
+                // Update the display
+                updateStatus("Edited wiki item.");
+                clearFields();
+                updateDisplay();
+                return;
+            }
+
+            // No item was selected
+            updateStatus("Unable to update wiki item. No item selected.");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int selectedIndex;
+
+            // Get the current selected index
+            if (listViewDisplay.SelectedIndices.Count > 0)
+            {
+                // What index in the list has the user selected
+                selectedIndex = listViewDisplay.SelectedIndices[0];
+
+                // Confirm the user meant to hit delete
+                DialogResult dialogResult = MessageBox.Show("Are you sure you wish to delete this item?",
+                    "Warning", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    // Clear the fields
+                    wiki[selectedIndex, 0] = "";
+                    wiki[selectedIndex, 1] = "";
+                    wiki[selectedIndex, 2] = "";
+                    wiki[selectedIndex, 3] = "";
+
+                    // Update the display
+                    updateStatus("Deleted wiki item.");
+                    clearFields();
+                    updateDisplay();
+                }
+
+                return;
+            }
+
+            // No item was selected
+            updateStatus("Unable to delete wiki item. No item selected.");
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // Open file to save data to
+            using (var stream = File.Open(fileName, FileMode.Create))
+            {
+                // Create a binary writer to write data to the file
+                using (var writer = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    // Write each wiki entry to the file
+                    for (int row = 0; row < rows; row++)
+                    {
+                        writer.Write(wiki[row, 0]);
+                        writer.Write(wiki[row, 1]);
+                        writer.Write(wiki[row, 2]);
+                        writer.Write(wiki[row, 3]);
+                    }
+                }
+            }
+
+            updateStatus("Saving to " + fileName + " complete.");
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            // Check if the file exists
+            if (File.Exists(fileName))
+            {
+                // Open the file
+                using (var stream = File.Open(fileName, FileMode.Open))
+                {
+                    // Create a reader to read from the file stream
+                    using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+                    {
+                        // Read each item from the file
+                        for (int row = 0; row < rows; row++)
+                        {
+                            wiki[row, 0] = reader.ReadString();
+                            wiki[row, 1] = reader.ReadString();
+                            wiki[row, 2] = reader.ReadString();
+                            wiki[row, 3] = reader.ReadString();
+                        }
+                    }
+                }
+
+                // Update display
+                updateStatus("Reading file '" + fileName + "' complete.");
+                updateDisplay();
+            }
+            else
+            {
+                updateStatus("Unable to open file '" + fileName + "'. File does not exist.");
+            }
+        }
+
+        private void swap(int index)
+        {
+            // Swap index and index + 1
+            string name = wiki[index, 0];
+            string category = wiki[index, 1];
+            string structure = wiki[index, 2];
+            string definition = wiki[index, 3];
+
+            // Swap row at index with index + 1
+            wiki[index, 0] = wiki[index + 1, 0];
+            wiki[index, 1] = wiki[index + 1, 1];
+            wiki[index, 2] = wiki[index + 1, 2];
+            wiki[index, 3] = wiki[index + 1, 3];
+
+            // Set row at index + 1 with the data from row at index
+            wiki[index + 1, 0] = name;
+            wiki[index + 1, 1] = category;
+            wiki[index + 1, 2] = structure;
+            wiki[index + 1, 3] = definition;
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            // Bubble sort the array
+            // Sorting by name means we target column index 0
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int swapIndex = 0; swapIndex < rows - 1 - row; swapIndex++)
+                {
+                    // Check if either entry is empty
+                    if (string.IsNullOrEmpty(wiki[swapIndex, 0]) || string.IsNullOrEmpty(wiki[swapIndex + 1, 0]))
+                    {
+                        // Count empty strings as large so they end up on the bottom
+                        continue;
+                    }
+                    else if (string.Compare(wiki[swapIndex, 0], wiki[swapIndex + 1, 0]) == 1)
+                    {
+                        swap(swapIndex);
+                    }
+                }
+            }
+
+            // Sorting is complete, update display
+            updateStatus("Wiki sorted by name.");
+            updateDisplay();
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            // validate the search term
+            string target = textBoxName.Text;
+
+            if (string.IsNullOrEmpty(target))
+            {
+                updateStatus("Input is invalid.");
+            }
+
+            // target is a valid choice, search for it with binary search
+            int mid;
+            int first = 0;
+            int last = rows;
+
+            int comparisonResult;
+
+            while (first <= last)
+            {
+                mid = (first + last) / 2;
+                
+                // Account for possible empty spots in the sorted array
+                if (string.IsNullOrEmpty(wiki[mid, 0]))
+                {
+                    last = mid - 1;
+                    continue;
+                }
+
+                comparisonResult = string.Compare(wiki[mid, 0], target);
+
+                // Check for match
+                if (comparisonResult == 0)
+                {
+                    updateStatus("Found target value.");
+                    listViewDisplay.Items[mid].Selected = true;
+                    displayFieldData(mid);
+                    return;
+                }
+
+                // Move the first and last if applicable
+                if (comparisonResult == 1)
+                {
+                    last = mid - 1;
+                }
+                else
+                {
+                    first = mid + 1;
+                }
+            }
+
+            updateStatus("Target not found in wiki.");
         }
     }
 }
